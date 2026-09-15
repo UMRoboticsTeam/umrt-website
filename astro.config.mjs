d9a6381c1f93677b0630c3f27a170dc6ec49ad25
Connor Pagtakhan <cepagtakhan@gmail.com>
2026-09-15 00:42:35 -0500

    Added mdx package, astro config updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import mdx from '@astrojs/mdx';
+
 export default defineConfig({
   site: 'https://umrt.ca',
   base: '/',
-  build: { format: 'directory' }
+  build: { format: 'directory' },
+  integrations: [mdx()]
 });
